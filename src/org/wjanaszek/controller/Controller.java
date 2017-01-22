eb9d1ed03fa7adcad60866e3c99e7d12a4267c9a
wjanaszek <wjanaszek@wjanaszek-Inspiron-3521>
2017-01-23 00:10:01 +0100

    Added GUI, controller update (better variables managment)

--- src/org/wjanaszek/controller/Controller.java
+++ src/org/wjanaszek/controller/Controller.java
@@ -2,10 +2,26 @@ package org.wjanaszek.controller;
 
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Stack;
 
 import org.wjanaszek.view.*;
+
+import com.sun.prism.paint.Color;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
 import org.wjanaszek.exceptions.EmptyInputException;
 import org.wjanaszek.exceptions.OperationException;
 import org.wjanaszek.exceptions.SpaceException;
@@ -27,6 +43,7 @@ public class Controller implements ReversePolishNotation {
 	private boolean variableValueChanged = false;
 	private char changedVariableName;
 	private char variableName;
+	private String input;
 	
 	/*
 	 * Konstruktor. Przyjmuje i ustawia odniesienia do widoku i modelu, aby moc sie z nimi komunikowac.
@@ -91,6 +108,8 @@ public class Controller implements ReversePolishNotation {
 					if(i == 0 && model.getVariablesHashMap().containsKey(c)){
 						if(i + 1 < inpt.length && inpt[i + 1] == '='){
 							model.getVariablesHashMap().remove(c);
+							variableValueChanged = true;
+							changedVariableName = inpt[0];
 							// usun formule definiujaca zmienna
 						}
 					}
@@ -135,6 +154,7 @@ public class Controller implements ReversePolishNotation {
 		char[] inpt = input.toCharArray();
 		int leftBrackets = 0;
 		int rightBrackets = 0;
+		int i = 0;
 		
 		if(input.length() == 0){
 			throw new EmptyInputException();
@@ -150,6 +170,10 @@ public class Controller implements ReversePolishNotation {
 			if(c == ' '){
 				throw new SpaceException();
 			}
+			if(i == 0 && Character.isAlphabetic(inpt[0]) && c != '=' && i == 1){
+				throw new IOException("Blad - dzialanie musi byc na 2 elementach!");
+			}
+			i++;
 		}
 		if(leftBrackets != rightBrackets){
 			throw new IOException("Brak poprawnie zapisanych nawiasów");
@@ -163,11 +187,9 @@ public class Controller implements ReversePolishNotation {
 		String tmp;
 		while(!model.getQueue().isEmpty()){
 			tmp = (String) model.getQueue().poll();
-			//System.out.println("tmp = " + tmp);
 			char[] element = tmp.toCharArray();
 			if(Character.isDigit(element[0])){
 				stack.push(ComplexNumber.valueOf(tmp));
-				//System.out.println("Dodaje do stosu: (" + tmp + "), " + ComplexNumber.valueOf(tmp).toString());
 			}
 			else if(stack.size() >= 2){
 				Operation operation = setOperation(element[0]);
@@ -202,8 +224,15 @@ public class Controller implements ReversePolishNotation {
 		}
 		ComplexNumber result;
 		result = stack.pop();
+		if(variableValueChanged){
+			//System.out.println("nazwa zmienionej zmiennej: " + changedVariableName);
+			//model.getVariablesHashMap().remove(changedVariableName);
+			model.getVariablesHashMap().put(changedVariableName, result);
+			//variableValueChanged = false;
+		}
 		if(variableDefinition){			// jesli byla definicja zmiennej, to dodaj ja do hash mapy zmiennych w modelu
 			model.getVariablesHashMap().put(variableName, result);
+			//variableDefinition = false;
 		}
 		return result;			// zwroc wynik
 	}
@@ -232,17 +261,17 @@ public class Controller implements ReversePolishNotation {
 	}
 	
 	private boolean isNumber(char[] input){
-		//System.out.println("isnumber");
 		for(char c: input){
-			//System.out.println(c);
 			if(!Character.isDigit(c) && c != '\n' && c != '.' && c != 'i'){
-				//System.out.println("c: " + c + "false");
 				return false;
 			}
 		}
 		return true;
 	}
 	
+	/* 
+	 * Jesli zmienna znajduje sie w modelu, to dodaj jej wartosc do kolejki, w przeciwnym razie zakomunikuj definicje nowej zmiennej 
+	*/
 	private void foundVariable(char c, int i){
 		if(model.getVariablesHashMap().containsKey(c)){			// zmien wartosc zmiennej
 			model.getQueue().add(model.getVariablesHashMap().get(c).toString());
@@ -326,9 +355,132 @@ public class Controller implements ReversePolishNotation {
 		}
 	}
 	
+	/* 
+	 * Metoda do obliczania wyrazenia w input i wyswietlania go w polu tekstowym okna
+	 * @param input wejsciowe wyrazenie, ktore obliczamy
+	 */
+	private void doWork(String input){
+		char[] entry;
+		boolean easyResult = false;
+		ComplexNumber result = new ComplexNumber();
+		try {
+			checkInput(input);
+		}
+		catch(IOException e0){
+			System.out.println(e0.toString());
+			return;
+		}
+		catch(SpaceException e1){
+			System.out.println(e1.toString());
+			return;
+		}
+		catch(EmptyInputException ei){
+			System.out.println(ei.toString());
+			return;
+		}
+		
+		if(countCharsInInput(input) > 1){
+			model.getVariableFormules().add(input);
+		}
+		
+		entry = input.toCharArray();
+		if(entry[0] == 'q'){
+			return;
+		}
+		if(input.length() == 1){
+			try {
+				easyRes(entry[0]);
+			}
+			catch(VariableNotFoundException vnf){
+				System.out.println(vnf.toString());
+				return;
+			}
+			result = model.getVariablesHashMap().get(entry[0]);
+			easyResult = true;
+		}
+		if (!easyResult) {
+			try {
+				parse(input);
+			} catch (VariableNotFoundException ve) {
+				System.out.println(ve.toString());
+				return;
+			}
+			try {
+				result = calculate();
+			} catch (OperationException e2) {
+				System.out.println(e2.toString());
+				return;
+			}
+			boolean wasRemoved = false;
+			if(variableValueChanged){
+				for(String s: model.getVariableFormules()){
+					if(!s.equals(input)){
+						for(int i = 0; i < model.getVariableFormules().size(); i++){
+							if(model.getVariableFormules().toArray()[i].toString().toCharArray()[0] == changedVariableName){
+								model.getVariableFormules().remove(i);
+								wasRemoved = true;
+							}
+						}
+					}
+					if(!wasRemoved) {
+						try {
+							parse(s);
+						} catch (VariableNotFoundException ve) {
+							System.out.println(ve.toString());
+						}
+						try {
+							result = calculate();
+							System.out.println("usuwam klucz " + s.toCharArray()[0]
+									+ ", by wrzucic go z powrotem z wartoscia " + result.toString());
+							model.getVariablesHashMap().remove(s.toCharArray()[0]);
+							model.getVariablesHashMap().put(s.toCharArray()[0], result);
+							//System.out.println("Wynik = " + result);
+						} catch (OperationException e2) {
+							System.out.println(e2.toString());
+							return;
+						} 
+					}
+					else {
+						break;
+					}
+				}
+				variableValueChanged = false;
+			}
+		}
+		if (variableDefinition) {
+			variableDefinition = false;
+			getText().setText("Dodano zmienna " + variableName);
+		} else {
+			//System.out.println("=" + result);
+			getText().setText(result.toString());
+		} 
+		easyResult = false;
+		return;
+	}
+	
+	/*
+	 * Zapewnia dzialanie programu w konsoli. Dziala w petli konczacej sie wprowadzeniem klawisza 'q'. 
+	 * Algorytm:
+	 * 1. Wczytaj wejscie
+	 * 2. Sprawdz, czy jest prawidlowe (tzn. czy nie zawiera bialych znakow itp.)
+	 * 3. Sparsuj je do modelu
+	 * 4. Oblicz wyrażenie wedlug danych z modelu
+	 * 5. Jeśli byla definicja nowej zmiennej lub ktoras z zadeklarowanych zmiennych zostala zmieniona, to wykonaj potrzebne obliczenia, 
+	 * 	  aby zaktualizowac jej wartosc w modelu.
+	 * 6. Wroc do pkt 1
+	 */
 	public void start() {
-		//view.main(null);
-		Scanner sc = new Scanner(System.in);
+		/* Stworz widok */
+		createRequiredListeners();
+		view.getShell().open();
+		view.getShell().layout();
+		while (!view.getShell().isDisposed()) {
+			if (!view.getDisplay().readAndDispatch()) {
+				view.getDisplay().sleep();
+			}
+		}
+
+		/*Scanner sc = new Scanner(System.in);
 		System.out.println("Witamy w kalkulatorze liczb zepsolonych. Milego uzytkowania!");
 		String input = sc.nextLine();
 		char[] entry;
@@ -377,10 +529,6 @@ public class Controller implements ReversePolishNotation {
 				easyResult = true;
 			}
 			if (!easyResult) {
-				/*if (Character.isAlphabetic(entry[0])) {
-					model.getVariableFormules().add(input);
-					//System.out.println(model.getVariableFormules().size());
-				}*/
 				try {
 					parse(input);
 				} catch (VariableNotFoundException ve) {
@@ -395,33 +543,41 @@ public class Controller implements ReversePolishNotation {
 					input = sc.nextLine();
 					continue;
 				}
-				char[] tmpChar;
-				// wersja teoretycznie lepsza
-				/*if (variableValueChanged) {
-					for (String s : model.getVariableFormules()) {
-						tmpChar = s.toCharArray();
-						for (int i = 0; i < tmpChar.length; i++) {
-							if (tmpChar[i] == changedVariableName) {
-								//System.out.println(s + "jest " + changedVariableName);
-							}
-						}
-					}
-				}*/
+				boolean wasRemoved = false;
 				if(variableValueChanged){
 					for(String s: model.getVariableFormules()){
-						try {
-							parse(s);
-						} catch (VariableNotFoundException ve) {
-							System.out.println(ve.toString());
+						if(!s.equals(input)){
+							for(int i = 0; i < model.getVariableFormules().size(); i++){
+								if(model.getVariableFormules().toArray()[i].toString().toCharArray()[0] == changedVariableName){
+									model.getVariableFormules().remove(i);
+									wasRemoved = true;
+								}
+							}
 						}
-						try {
-							calculate();
-						} catch (OperationException e2) {
-							System.out.println(e2.toString());
-							input = sc.nextLine();
-							continue;
+						if(!wasRemoved) {
+							try {
+								parse(s);
+							} catch (VariableNotFoundException ve) {
+								System.out.println(ve.toString());
+							}
+							try {
+								result = calculate();
+								System.out.println("usuwam klucz " + s.toCharArray()[0]
+										+ ", by wrzucic go z powrotem z wartoscia " + result.toString());
+								model.getVariablesHashMap().remove(s.toCharArray()[0]);
+								model.getVariablesHashMap().put(s.toCharArray()[0], result);
+								//System.out.println("Wynik = " + result);
+							} catch (OperationException e2) {
+								System.out.println(e2.toString());
+								input = sc.nextLine();
+								continue;
+							} 
+						}
+						else {
+							break;
 						}
 					}
+					variableValueChanged = false;
 				}
 			}
 			if (variableDefinition) {
@@ -429,7 +585,19 @@ public class Controller implements ReversePolishNotation {
 			} else {
 				System.out.println("=" + result);
 			} 
-			easyResult = false;
+			
+			/*System.out.println("Zawartosc formul:");
+			for(int i = 0; i < model.getVariableFormules().size(); i++){
+				System.out.println(model.getVariableFormules().toArray()[i]);
+			}
+			
+			System.out.println("Zawartosc mapy:");
+			for (Entry<Character, ComplexNumber> entry1 : model.getVariablesHashMap().entrySet()) {
+			    System.out.println(entry1.getKey() + " : " + entry1.getValue());
+			}
+			System.out.println("------------------------------");*/
+			
+			/*easyResult = false;
 			input = sc.nextLine();
 		}
 		/*LinkedList<String> queue = model.getQueue();
@@ -441,7 +609,276 @@ public class Controller implements ReversePolishNotation {
 		/*System.out.println(new ComplexNumber(2, -3).toString());
 		System.out.println(ComplexNumber.valueOf("2+3i").toString());*/
 		
-		System.out.println("Koniec");
+		/*System.out.println("Koniec");*/
 	}
 	
+	/* Delegaci: */
+	private Text getText(){
+		return view.getText();
+	}
+	
+	private Button getButton1() {
+		return view.getButton1();
+	}
+
+	private Button getButton2() {
+		return view.getButton2();
+	}
+
+	private Button getButton3() {
+		return view.getButton3();
+	}
+
+	private Button getButton4() {
+		return view.getButton4();
+	}
+
+	private Button getButton5() {
+		return view.getButton5();
+	}
+
+	private Button getButton6() {
+		return view.getButton6();
+	}
+
+	private Button getButton7() {
+		return view.getButton7();
+	}
+
+	private Button getButton8() {
+		return view.getButton8();
+	}
+
+	private Button getButton9() {
+		return view.getButton9();
+	}
+
+	private Button getButton0() {
+		return view.getButton0();
+	}
+
+	private Button getButtonDot() {
+		return view.getButtonDot();
+	}
+
+	private Button getButtonPls() {
+		return view.getButtonPls();
+	}
+
+	private Button getButtonSub() {
+		return view.getButtonSub();
+	}
+
+	private Button getButtonMul() {
+		return view.getButtonMul();
+	}
+
+	private Button getButtonDiv() {
+		return view.getButtonDiv();
+	}
+
+	private Button getButtonEqa() {
+		return view.getButtonEqa();
+	}
+
+	private Button getButtonEnt() {
+		return view.getButtonEnt();
+	}
+
+	private Button getButtonI() {
+		return view.getButtonI();
+	}
+	
+	private Button getButtonRes() {
+		return view.getButtonRes();
+	}
+	
+	/*
+	 * Stworz potrzebnych "nasluchiwaczy" przyciskow 
+	 */
+	private void createRequiredListeners(){
+		view.open();
+
+		getButton1().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("1");
+				}				
+			}		
+		});
+		getButton2().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("2");
+				}			
+			}			
+		});
+		getButton3().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("3");
+				}				
+			}			
+		});
+		getButton4().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("4");
+				}				
+			}			
+		});
+		getButton5().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("5");
+				}				
+			}		
+		});
+		getButton6().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("6");
+				}				
+			}		
+		});
+		getButton7().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("7");
+				}				
+			}			
+		});
+		getButton8().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("8");
+				}				
+			}			
+		});
+		getButton9().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("9");
+				}				
+			}			
+		});
+		getButton0().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("0");
+				}				
+			}			
+		});
+		getButtonDot().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert(".");
+				}				
+			}			
+		});
+		getButtonPls().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("+");
+				}				
+			}		
+		});
+		getButtonSub().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("-");
+				}			
+			}			
+		});
+		getButtonMul().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("*");
+				}				
+			}		
+		});
+		getButtonDiv().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("/");
+				}		
+			}	
+		});
+		getButtonEqa().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("=");
+				}
+			}
+		});
+		getButtonI().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					getText().insert("i");
+				}	
+			}
+		});
+		getButtonEnt().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e) {
+				switch(e.type){
+				case SWT.Selection:
+					//System.out.println("Licz!");
+					doWork(getText().getText());
+				}			
+			}
+		});
+		getText().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e){
+				switch(e.type){
+				case SWT.Selection:
+					System.out.println("ENTER");
+				}
+			}
+		});
+		getButtonRes().addListener(SWT.Selection, new Listener(){
+			@Override
+			public void handleEvent(Event e){
+				switch(e.type){
+				case SWT.Selection:
+					getText().setText("");
+				}
+			}
+		});
+	}
 }
--- src/org/wjanaszek/view/View.java
+++ src/org/wjanaszek/view/View.java
@@ -1,31 +1,270 @@
 package org.wjanaszek.view;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
-public class View extends Application {
-	public static void main(String args[]){
-		launch(args);
+public class View {
+	private static final String APP_NAME = "ComplexNumberCalc";
+	private Text text;
+	private Button button1,
+					button2,
+					button3,
+					button4,
+					button5,
+					button6,
+					button7,
+					button8,
+					button9,
+					button0,
+					buttonDot,
+					buttonPls,
+					buttonSub,
+					buttonMul,
+					buttonDiv,
+					buttonArg,
+					buttonAbs,
+					buttonEqa,
+					buttonEnt,
+					buttonI,
+					buttonRes;
+	private Display display;
+	private Shell shell;
+	
+	public Display getDisplay(){
+		return display;
+	}
+	
+	public Shell getShell(){
+		return shell;
+	}
+	
+	public Text getText() {
+		return text;
 	}
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+
+	public Button getButton1() {
+		return button1;
+	}
+
+
+	public Button getButton2() {
+		return button2;
+	}
+
+
+	public Button getButton3() {
+		return button3;
+	}
+
+
+	public Button getButton4() {
+		return button4;
+	}
+
+
+	public Button getButton5() {
+		return button5;
+	}
+
+
+	public Button getButton6() {
+		return button6;
+	}
+
+
+	public Button getButton7() {
+		return button7;
+	}
+
+
+	public Button getButton8() {
+		return button8;
+	}
+
+
+	public Button getButton9() {
+		return button9;
+	}
+
+
+	public Button getButton0() {
+		return button0;
+	}
+
+
+	public Button getButtonDot() {
+		return buttonDot;
+	}
+
+
+	public Button getButtonPls() {
+		return buttonPls;
+	}
+
+
+	public Button getButtonSub() {
+		return buttonSub;
+	}
+
+
+	public Button getButtonMul() {
+		return buttonMul;
+	}
+
+
+	public Button getButtonDiv() {
+		return buttonDiv;
+	}
+
+
+	public Button getButtonArg() {
+		return buttonArg;
+	}
+
+
+	public Button getButtonAbs() {
+		return buttonAbs;
+	}
+
+
+	public Button getButtonEqa() {
+		return buttonEqa;
+	}
+
+
+	public Button getButtonEnt() {
+		return buttonEnt;
+	}
+
+
+	public Button getButtonI() {
+		return buttonI;
+	}
+
+	public Button getButtonRes() {
+		return buttonRes;
+	}
+
+	/**
+	 * Open the window.
+	 * @wbp.parser.entryPoint
+	 */
+	public void open() {
+		display = Display.getDefault();
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText(APP_NAME);
+		shell.setLayout(null);
 		
+		text = new Text(shell, SWT.BORDER);
+		text.setBounds(64, 10, 165, 22);
 		
+		button1 = new Button(shell, SWT.NONE);
+		/*button1.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseDown(MouseEvent e){
+				System.out.println("Wcisnieto 1");
+			}
+			
+			@Override
+			public void mouseUp(MouseEvent e){
+				System.out.println("Wycisnieto 1");
+			}
+		});*/
+		button1.setBounds(10, 51, 82, 22);
+		button1.setText("1");
+		
+		button2 = new Button(shell, SWT.NONE);
+		button2.setBounds(98, 51, 82, 22);
+		button2.setText("2");
+		
+		button3 = new Button(shell, SWT.NONE);
+		button3.setBounds(186, 51, 82, 22);
+		button3.setText("3");
+		
+		button4 = new Button(shell, SWT.NONE);
+		button4.setBounds(10, 79, 82, 22);
+		button4.setText("4");
+		
+		button5 = new Button(shell, SWT.NONE);
+		button5.setBounds(98, 79, 82, 22);
+		button5.setText("5");
+				
+		button6 = new Button(shell, SWT.NONE);
+		button6.setBounds(186, 79, 82, 22);
+		button6.setText("6");
+		
+		button7 = new Button(shell, SWT.NONE);
+		button7.setBounds(10, 107, 82, 22);
+		button7.setText("7");
+		
+		button8 = new Button(shell, SWT.NONE);
+		button8.setBounds(98, 107, 82, 22);
+		button8.setText("8");
+		
+		button9 = new Button(shell, SWT.NONE);
+		button9.setBounds(186, 107, 82, 22);
+		button9.setText("9");
+				
+		button0 = new Button(shell, SWT.NONE);
+		button0.setBounds(10, 135, 82, 22);
+		button0.setText("0");
+		
+		buttonDot = new Button(shell, SWT.NONE);
+		buttonDot.setBounds(98, 135, 82, 22);
+		buttonDot.setText(".");
+		
+		buttonPls = new Button(shell, SWT.NONE);
+		buttonPls.setBounds(186, 135, 82, 22);
+		buttonPls.setText("+");
+		
+		buttonSub = new Button(shell, SWT.NONE);
+		buttonSub.setBounds(274, 51, 82, 22);
+		buttonSub.setText("-");
+		
+		buttonMul = new Button(shell, SWT.NONE);
+		buttonMul.setBounds(274, 79, 82, 22);
+		buttonMul.setText("*");
+		
+		buttonDiv = new Button(shell, SWT.NONE);
+		buttonDiv.setBounds(274, 107, 82, 22);
+		buttonDiv.setText("/");
+		
+		buttonEqa = new Button(shell, SWT.NONE);
+		buttonEqa.setBounds(274, 135, 82, 22);
+		buttonEqa.setText("=");
+		
+		buttonArg = new Button(shell, SWT.NONE);
+		buttonArg.setBounds(10, 163, 82, 22);
+		buttonArg.setText("arg");
+		
+		buttonAbs = new Button(shell, SWT.NONE);
+		buttonAbs.setBounds(98, 163, 82, 22);
+		buttonAbs.setText("abs");
+		
+		buttonI = new Button(shell, SWT.NONE);
+		buttonI.setBounds(186, 163, 82, 22);
+		buttonI.setText("i");
+		
+		buttonEnt = new Button(shell, SWT.NONE);
+		buttonEnt.setBounds(274, 163, 82, 22);
+		buttonEnt.setText("ENTER");
+		
+		buttonRes = new Button(shell, SWT.NONE);
+		buttonRes.setBounds(262, 10, 82, 22);
+		buttonRes.setText("RESET");
+		
+		Label lblWejcie = new Label(shell, SWT.NONE);
+		lblWejcie.setBounds(10, 16, 48, 16);
+		lblWejcie.setText("wejście:");
 	}
 }
